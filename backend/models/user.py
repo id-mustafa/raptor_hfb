3bc1f9885523bcca94257b4c87f0ad0976776a10
Mustafa Aljumayli <idmustafase@gmail.com>
2025-09-27 22:38:01 -0400

    trying to join room

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,7 +1,12 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .room import Room
 
 
 class User(SQLModel, table=True):
     username: str = Field(primary_key=True)
-    room_id: int = Field(default=None)
+    room_id: int = Field(default=None, foreign_key="room.id")
     tokens: str = Field(default=0)
+    room: "Room" = Relationship(back_populates="players")
